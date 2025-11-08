76e2af82a8c77017083cab0baf686ee847d9f21f
2isu <simasturbo0577@gmail.com>
2025-11-08 17:33:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/seu-repositorio/', // ALTERE para o nome do seu repositório no GitHub
+  base: '/comunity-hub-prototype/', // ALTERE para o nome do seu repositório no GitHub
   server: {
     host: "::",
     port: 8080,
